4073c543bec2861703548f6bf9702b830cd5eb49
Aahmed-Hossain <aahmdctec@gmail.com>
2023-11-30 08:21:38 +0600

    interceptor implemented successfully

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,9 +1,24 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const axiosSecure = axios.create({
     baseURL: `http://localhost:5000`, withCredentials: true
 })
 const useAxiosSecure = () => {
+    const navigate = useNavigate();
+    axiosSecure.interceptors.response.use(response=> response, async error=> {
+        console.log('error in interceptors', error.response);
+        if(error.response && error.response.status ===401 || error.response.status ===403){
+            await axiosSecure.get('/logout');
+            navigate('/login')
+        }
+        return Promise.reject(error);
+    })
+
+
+
+
+
     return axiosSecure;
 };
 
